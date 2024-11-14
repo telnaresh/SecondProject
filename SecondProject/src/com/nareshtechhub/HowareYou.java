a8e3c8a30c084464db97263bc1ae656e2b03d906
T.Naresh <tellapuri.naresh@gmail.com>
2024-11-14 22:23:56 +0530

    char value return

--- SecondProject/src/com/nareshtechhub/HowareYou.java
+++ SecondProject/src/com/nareshtechhub/HowareYou.java
@@ -8,6 +8,11 @@ public class HowareYou {
 	}
 	public static  void m1() {
 		System.out.println("m1 method");
+		System.out.println(add());
+	}
+	
+	public static int add() {
+		return 'a';
 	}
 
 }
